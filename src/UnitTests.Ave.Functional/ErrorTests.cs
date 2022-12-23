83197b78ec1e0ad0f0a1400f122cbb0ca9addf8a
aadversteeg <aad@versteeg-its.nl>
2022-12-23 17:24:30 +0100

    Added validation on error construction parameters

--- src/UnitTests.Ave.Functional/ErrorTests.cs
+++ src/UnitTests.Ave.Functional/ErrorTests.cs
@@ -1,13 +1,58 @@
+using Ave.Extensions.Functional;
+using FluentAssertions;
+using System;
 using Xunit;
 
 namespace UnitTests.Ave.Functional
 {
 	public class ErrorTests
 	{
-		[Fact]
-		public void Test1()
+		[Fact(DisplayName ="E-0001: Creating an error without a message should fail")]
+		public void E0001()
 		{
+			// arrange
 
+			// act
+			var act = () => new Error("code", null);
+
+			// assert
+			act.Should().Throw<ArgumentNullException>();
 		}
-	}
+
+        [Fact(DisplayName = "E-0002: Creating an error without a code should fail")]
+        public void E0002()
+        {
+            // arrange
+
+            // act
+            var act = () => new Error(null, "message");
+
+            // assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "E-0003: Creating an error without a message should fail")]
+        public void E0003()
+        {
+            // arrange
+
+            // act
+            var act = () => new Error("code", "");
+
+            // assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "E-0004: Creating an error without an empty code should fail")]
+        public void E0004()
+        {
+            // arrange
+
+            // act
+            var act = () => new Error("", "message");
+
+            // assert
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
 }
