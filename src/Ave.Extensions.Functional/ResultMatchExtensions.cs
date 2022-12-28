37c82c3f9e90bc52ef7b7ffee5c38e4cc9f7d0e3
Aad Versteeg <aad.versteeg@transavia.com>
2022-12-28 14:14:24 +0100

    Added Result Match

---
+++ src/Ave.Extensions.Functional/ResultMatchExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ave.Extensions.Functional
+{
+	public static class ResultMatchExtensions
+	{
+		public static Result<TOut, E> Match<TIn, TOut, E>(this Result<TIn, E> source, Func<TIn, TOut> onSuccess, Func<E, TOut> onError)
+		{
+			if(source.IsSuccess) {
+				return Result<TOut, E>.Success(onSuccess(source.Value));
+			}
+
+			return Result<TOut, E>.Success(onError(source.Error));
+		}
+
+		public static async Task<Result<TOut, E>> Match<TIn, TOut, E>(this Task<Result<TIn, E>>  awaitableSource, Func<TIn, TOut> onSuccess, Func<E, TOut> onError)
+		{
+			var source = await awaitableSource.ConfigureAwait(false);
+			if (source.IsSuccess)
+			{
+				return Result<TOut, E>.Success(onSuccess(source.Value));
+			}
+
+			return Result<TOut, E>.Success(onError(source.Error));
+		}
+	}
+}
